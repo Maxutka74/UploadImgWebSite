81ce3d703df2722d04e37c1c65acaa3c0ee3517b
Maxutka74 <gouynoran@gmail.com>
2025-11-04 20:46:21 +0200

    git commit

---
+++ frontend/js/index.js
@@ -0,0 +1,10 @@
+const allImgBlocks = document.querySelectorAll('.hero__image');
+const randomIndex = Math.floor(Math.random() * allImgBlocks.length)
+const randomBlock = allImgBlocks[randomIndex]
+randomBlock.classList.add('is-visible')
+
+document.body.style.setProperty('background-color', '#151515')
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const showcaseButton = document.querySelector('.header__button-btn')
+// })
